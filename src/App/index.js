3f82b0500979522dee54598903ee2ed32b6a99e6
Dani Crosby <dani.crosby1111@gmail.com>
2021-04-21 14:38:55 -0500

    added more form info, needs logic on buttons

--- src/App/index.js
+++ src/App/index.js
@@ -12,11 +12,11 @@ import {
 } from 'reactstrap';
 
 function App() {
-  const [domWriting, setDomWriting] = useState('Nothing Here!');
+  const [domWriting, setDomWriting] = useState('Add State Logic');
 
   const handleClick = (e) => {
     console.warn(`You clicked ${e.target.id}`);
-    setDomWriting(`You clicked ${e.target.id}! Check the Console!`);
+    setDomWriting(`You clicked ${e.target.id}! Now Write The Logic!`);
   };
 
   return (
@@ -30,9 +30,7 @@ function App() {
             <FormGroup>
               <Label for="exampleNumber"></Label>
               <Input type="number" name="number" id="exampleNumber" placeholder="ZipCode, City, etc"/>
-            <div>
-              <Button id='this-button' className='btn btn-success' onClick={handleClick}>Submit</Button>
-            </div>
+              <div><Button id='this-button' className='btn btn-success' onClick={handleClick}>Submit</Button></div>
             </FormGroup>
           </Form>
         </CardBody>
